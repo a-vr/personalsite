eedf8b7235160a5d58bc3302158e5b89c4aa5664
a-vr <akanksha@ramabadran.com>
2025-03-18 10:49:19 -0400

    Update about.py March 2025

--- about.py
+++ about.py
@@ -6,25 +6,25 @@ def app():
     # opening para
 
     # use lottie file
-    def loadlottie(url):
-        r = requests.get(url)
-        if r.status_code != 200: 
-            return None
-        return r.json()
+    # def loadlottie(url):
+        # r = requests.get(url)
+        # if r.status_code != 200: 
+            # return None
+        # return r.json()
 
     with st.container():
         text_col, img_col = st.columns((3,2))
         with text_col:
-            st.title("Hi! I'm Akanksha")
-            st.write("""I'm a passionate software engineer with a background in astrophysics and automation. 
-                     With hands-on experience in software development, analytical research, and technical project management, I'm eager to apply 
-                     my skills to creative new initiatives.""")
-            st.write("""Please feel free to reach out to me via the Contact page or [LinkedIn](https://www.linkedin.com/in/akanksha-ramabadran/) 
-                     if you would like get in touch!""")
-        with img_col:
+            st.title("About Me")
+            st.write("""Hello, I'm Akanksha, a dedicated Software Engineer with a strong foundation in astrophysics and automation. I specialize in developing efficient, scalable software solutions and 
+            have a proven track record in analytical research and technical project management. My diverse background allows me to approach complex problems from unique perspectives, driving innovation in 
+            both software development and system optimization.""")
+            st.write("""I am always eager to collaborate on challenging projects and contribute my skills to solve real-world problems. Feel free to connect with me through the Contact page or 
+                    [LinkedIn](https://www.linkedin.com/in/akanksha-ramabadran/) if you're interested in working together or discussing opportunities.""")
+        # with img_col:
             # add headshot
-            lot = loadlottie("https://lottie.host/6aa16f90-6007-467f-b8c1-438d7f4d26dd/WDII2HGGD4.json")
-            st_lottie(lot, height=300)
+            # lot = loadlottie("https://lottie.host/6aa16f90-6007-467f-b8c1-438d7f4d26dd/WDII2HGGD4.json")
+            # st_lottie(lot, height=300)
 
     with st.container():
         st.write("---")
@@ -32,10 +32,11 @@ def app():
         st.write("")
         st.write(
             """
-            I specialize in software development with a focus on Python, C++, and UiPath, including:
-            - **Automation**: At my current position, my team works on enhancing and optimizing existing UiPath automations to effectively address business needs.
-            - **Web Development**: I also work on designing and developing web platforms, such as a blockchain-based Ferrari trading platform and this website, both powered by Streamlit!
-            - **Project Management**: I have lead multidisciplinary teams to execute complex projects, from designing Mars mission concepts to spearheading research in astronomy and beyond.
+            I specialize in software development with expertise in Python, C++, and UiPath, focusing on delivering efficient solutions in the following areas:
+            - **Automation**: In my current role at Wells Fargo, I work on optimizing and enhancing UiPath automation workflows to address evolving business needs, improving efficiency and scalability.
+            - **Data Analysis**:  I have hands-on experience in analyzing complex scientific datasets, utilizing Python and specialized libraries for data manipulation and visualization. My work includes developing 
+            models to analyze astronomical data, supporting research through data-driven insights, and applying statistical methods to interpret scientific phenomena.
+            - **Project Management**: I lead multidisciplinary teams in executing complex projects, from conceptualizing Mars mission designs to driving impactful research in astronomy and other scientific fields.
             
-            You can check out my Research and Projects pages to learn more!""")
+            To explore my work further, please visit the Research and Projects pages.""")
         
